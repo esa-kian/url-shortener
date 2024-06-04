743b2102e58588a315b5b46aee9b97673ee4b0fd
Esa <ehsan_amd72@hotmail.com>
2024-06-04 16:16:00 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type URLShortener struct {
+	urls map[string]string
+}
+
+func (us *URLShortener) HandleShorten(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	originalURL := r.FormValue("url")
+
+	if originalURL == "" {
+		http.Error(w, "URL parameter is missing", http.StatusBadRequest)
+		return
+	}
+
+	shortKey := generateShortKey()
+
+	us.urls[shortKey] = originalURL
+
+	shortenedURL := fmt.Sprintf("http://localhost:8080/short/%s", &shortKey)
+
+	w.Header().Set("Content-Type", "text/html")
+
+	responseHTML := fmt.Sprintf(`
+		<h2>URL Shortener</h2>
+		<p>Original URL: %s</p>
+		<p>Shortened URL: <a href="%s">%s</a></p>
+		<form method="post" action="/shorten">
+			<input type="text" name="url" placeholder="Enter a URL">
+			<input type="submit" value="Shorten">
+		</form>
+	`, originalURL, shortenedURL, shortenedURL)
+
+	fmt.Fprintf(w, responseHTML)
+}
+
+func (us *URLShortener) HandleRedirect(w http.ResponseWriter, r *http.Request) {
+	shortKey := r.URL.Path[len("/short/"):]
+
+	if shortKey == "" {
+		http.Error(w, "Shortened key is missing", http.StatusBadRequest)
+		return
+	}
+
+	originalURL, found := us.urls[shortKey]
+
+	if !found {
+		http.Error(w, "Shortened key not found", http.StatusNotFound)
+		return
+	}
+
+	http.Redirect(w, r, originalURL, http.StatusMovedPermanently)
+}
+
+func generateShortKey() string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	const keyLength = 6
+
+	rand.Seed(time.Now().UnixNano())
+
+	shortKey := make([]byte, keyLength)
+
+	for i := range shortKey {
+		shortKey[i] = charset[rand.Intn(len(charset))]
+
+	}
+
+	return string(shortKey)
+}
+
+func main() {
+	shortener := &URLShortener{
+		urls: make(map[string]string),
+	}
+
+	http.HandleFunc("/shorten", shortener.HandleShorten)
+	http.HandleFunc("/short/", shortener.HandleRedirect)
+
+	fmt.Println("URL Shortener is running on :8080")
+	http.ListenAndServe(":8080", nil)
+
+}
